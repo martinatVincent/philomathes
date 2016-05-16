5a09bfebd6fc23324b7f830927358af2c682519b
defelite <lanfranchi.guillaume@yahoo.com>
2016-05-16 12:05:53 +0200

    lien footer fonctionnel
    les icones du footer avec les bon lien

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -4,9 +4,9 @@
 		<div class="container">
 			<div class="col l6 m6 s6 divfooter">
 				<h5 class="ad-philo">Adresse : 66 Rue Abbé de l'Épée,<br> 33000 Bordeaux</h5>
-					<i class="fa fa-facebook-square fa-3x"></i>
-					<i class="fa fa-twitter-square fa-3x"></i>
-					<i class="fa fa-instagram fa-3x"></i>
+					<a href="https://www.facebook.com/Philomathique-de-Bordeaux-815911835182414/?rf=122992434421050"> <i class="fa fa-facebook-square fa-3x"></i></a>
+					<a href="https://twitter.com/hashtag/philomatique"> <i class="fa fa-twitter-square fa-3x"></i></a>
+					<a href="http://websta.me/tag/philomatique"><i class="fa fa-instagram fa-3x"></i></a>
 			</div>
 			<div class="right-align col l6 m6 s6 divfooter">
 				<ul>
